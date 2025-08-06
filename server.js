ba32ab1cf6f420552e5efc4477f2b95c48f09557
Brandon Bocardo <brandon@ejemplo.com>
2025-08-06 10:32:18 -0600

    cambio server.js

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const app = require('./src/app'); // ✅ Importa `app.js`
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-    console.log(`✅ Servidor corriendo en http://Localhost:${port}`);
+    console.log(`✅ Servidor corriendo n en http://Localhost:${port}`);
 });
